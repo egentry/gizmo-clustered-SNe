9a5cd12f693364452ca6bceb155c97e9134626aa
Eric Gentry <egentry@ucsc.edu>
2017-10-20 15:16:57 -0700

    add grackle support for visualizing

---
+++ scripts/grackle_helpers.py
@@ -0,0 +1,102 @@
+from __future__ import print_function, division
+
+import os
+
+from pygrackle import chemistry_data, FluidContainer
+
+from pygrackle.utilities.physical_constants import \
+    mass_hydrogen_cgs, \
+    sec_per_Myr, \
+    cm_per_mpc
+
+import yt
+
+
+def wrapped_initializer(mass_unit_cgs, length_unit_cgs, time_unit_cgs,
+                        verbose=False):
+    """ Create a `grackle.chemistry_data` 
+
+    """
+    if verbose:
+        print("""`grackle_helpers.wrapped_initializer` is setting up grackle assuming code units:
+    mass   : {} g
+    length : {} cm
+    time   : {} s
+""".format(mass_unit_cgs, length_unit_cgs, time_unit_cgs))
+
+    current_redshift = 0.
+
+    # Set solver parameters
+    my_chemistry = chemistry_data()
+    my_chemistry.use_grackle = 1
+    my_chemistry.with_radiative_cooling = 1
+    my_chemistry.primordial_chemistry = 0
+    my_chemistry.metal_cooling = 1
+    my_chemistry.UVbackground = 1
+    my_chemistry.self_shielding_method = 0
+    my_chemistry.H2_self_shielding = 0
+
+    grackle_dir = "/pfs/home/egentry/local/grackle"
+    my_chemistry.grackle_data_file = os.path.join(
+        grackle_dir, "input", "CloudyData_UVB=HM2012.h5")
+
+    print("grackle cooling file: ", my_chemistry.grackle_data_file)
+
+    # Set units
+    my_chemistry.comoving_coordinates = 0  # proper units
+    my_chemistry.a_units = 1.0
+    my_chemistry.a_value = 1. / (1. + current_redshift) / \
+        my_chemistry.a_units
+    my_chemistry.density_units = mass_unit_cgs / length_unit_cgs**3
+    my_chemistry.length_units = length_unit_cgs
+    my_chemistry.time_units = time_unit_cgs
+    my_chemistry.velocity_units = my_chemistry.a_units * \
+        (my_chemistry.length_units / my_chemistry.a_value) / \
+        my_chemistry.time_units
+
+    rval = my_chemistry.initialize()
+
+    return rval, my_chemistry
+
+
+def temperature(field, data, my_chemistry):
+
+    fc = FluidContainer(my_chemistry,
+                        data["PartType0", "Masses"].shape[0])
+
+    fc["density"][:] = data["PartType0", "Density"]
+    fc["metal"][:] = data["PartType0", "Density"] * data["PartType0",
+                                                         "Metallicity"]
+    fc["x-velocity"][:] = data["PartType0", "Velocities"][:, 0]
+    fc["y-velocity"][:] = data["PartType0", "Velocities"][:, 1]
+    fc["z-velocity"][:] = data["PartType0", "Velocities"][:, 2]
+    fc["energy"][:] = data["PartType0", "InternalEnergy"]
+
+    fc.calculate_temperature()
+
+    return fc["temperature"] * yt.units.Kelvin
+
+
+def cooling_rate(field, data, my_chemistry):
+
+    fc = FluidContainer(my_chemistry, data["PartType0", "Masses"].shape[0])
+
+    fc["density"][:] = data["PartType0", "Density"]
+    fc["metal"][:] = data["PartType0", "Density"] * data["PartType0", "Metallicity"]
+    fc["x-velocity"][:] = data["PartType0", "Velocities"][:,0]
+    fc["y-velocity"][:] = data["PartType0", "Velocities"][:,1]
+    fc["z-velocity"][:] = data["PartType0", "Velocities"][:,2]
+    fc["energy"][:] = data["PartType0", "InternalEnergy"]
+
+    fc.calculate_temperature()
+    fc.calculate_cooling_time()
+
+    dt = yt.units.s
+    fc.solve_chemistry(dt.value / my_chemistry.time_units)
+
+    de = (data["PartType0", "InternalEnergy"].value - fc["energy"])
+    de *= yt.units.Msun * yt.units.pc**2 / yt.units.Myr**2
+
+    de_dt = de / dt
+
+    return de_dt
--- scripts/visualize_helpers.py
+++ scripts/visualize_helpers.py
@@ -8,13 +8,63 @@ import seaborn as sns
 
 from units import M_solar, m_proton, pc, yr, Myr, gamma, km, s
 import MHD
+from grackle_helpers import wrapped_initializer, temperature, cooling_rate
 
 from matplotlib import pyplot as plt
 
 import warnings
 
 
+# ############# YT HELPERS ############
+
+@yt.derived_field(name="pressure", units="g  / s**2 / cm",
+                  sampling_type="particle")
+def _pressure(field, data):
+    return (gamma-1) * data["thermal_energy"] * data["density"]
+
+
+@yt.particle_filter(requires=["particle_velocity_magnitude"],
+                    filtered_type='all')
+def affected(pfilter, data):
+    filter = data[(pfilter.filtered_type,
+                  "particle_velocity_magnitude")].to("m/s") > 1
+    return filter
+
+# # Grackle-required fields
+# # Make sure these units are correct!!!
+rval, my_chemistry_particles = wrapped_initializer(
+    float(yt.units.Msun.to("g").value),
+    float(yt.units.pc.to("cm").value),
+    float(yt.units.Myr.to("s").value),
+    verbose=True,
+    )
+assert(rval == 1)
+
+# # bind `my_chemistry_particles` as a closure to `temperature`
+# # Note: yt has problems with using lambdas; better to use a real function
+def _temperature(field, data):
+    return temperature(field, data, my_chemistry_particles)
+
+yt.add_field(("all", "temperature"),
+             function=_temperature,
+             sampling_type="particle",
+             units="K",
+             force_override=True,
+             )
+
+# # bind `my_chemistry_particles` as a closure to `cooling_rate`
+# # Note: yt has problems with using lambdas; better to use a real function
+def _cooling_rate(field, data):
+    return cooling_rate(field, data, my_chemistry_particles)
+yt.add_field(("all", "cooling_rate"),
+             function=_cooling_rate,
+             sampling_type="particle",
+             units="Msun * pc**2 / Myr**3 ",
+             force_override=True,
+             )
+
 # #########################
+
 snapshot_filename_format = "snapshot_???.hdf5"
 
 
@@ -46,6 +96,15 @@ def load_snapshots(outputs_dir):
     ts = yt.load(os.path.join(outputs_dir, snapshot_filename_format),
                  unit_base=unit_base)
     return ts
+
+
+def load_ds_from_ts(ts, i):
+    """gets the `ith` snapshot from `ts`.
+    Allows automated binding of filters, etc
+    """
+    ds = ts[i]
+    ds.add_particle_filter("affected")
+    return ds
 
 
 def get_snapshot_time(snapshot_filename):
@@ -80,7 +139,8 @@ def total_radial_momentum_of_snapshot(snapshot_filename,
         r_hat = r_hat / (np.sum(r_hat**2, axis=1, dtype=float).reshape(new_shape))**.5
 
         mom = np.sum(r_hat * f[particle_type]["Velocities"]
-                     * np.reshape(f[particle_type]["Masses"], new_shape), dtype=float)
+                     * np.reshape(f[particle_type]["Masses"], new_shape),
+                     dtype=float)
 
     return mom * M_solar * pc / (Myr)
 
@@ -219,7 +279,7 @@ def plot_projected_density(ts, snapshot_number, snapshot_number_to_index_map,
     """
 
     i = snapshot_number_to_index_map[snapshot_number]
-    ds = ts[i]
+    ds = load_ds_from_ts(ts, i)
 
     p = yt.ProjectionPlot(ds, "x", ("gas", "density"))
     p.set_log('density', False)
@@ -286,6 +346,8 @@ def plot_sliced_field(ts, snapshot_number, snapshot_number_to_index_map, field,
     show_plot : Optional(bool)
         - True if you want to show plots
          (you might want to disable this for non-interactive sessions)
+    seaborn_style : Optional(str)
+        - a valid argument for `seaborn.axes_style`
     add_magnetic_field_lines : Optional(bool)
         - True if you want to try adding field lines to the plot
           (field lines must already be computed and saved to disk
@@ -293,12 +355,12 @@ def plot_sliced_field(ts, snapshot_number, snapshot_number_to_index_map, field,
 
     """
     i = snapshot_number_to_index_map[snapshot_number]
-    ds = ts[i]
+    ds = load_ds_from_ts(ts, i)
     s = yt.SlicePlot(ds, "x", (field_type[field], field))
 
     if field == "density":
         s.set_unit(field, "amu/cm**3")
-        s.set_zlim(field, 10**-3)
+        # s.set_zlim(field, 1e-3, 1e1)
         s.set_colorbar_label(field, "Density $ [ m_\mathrm{H} \; \mathrm{cm}^{-3} ] $")
 
     s.set_cmap(field=field, cmap="viridis")
@@ -352,8 +414,11 @@ def plot_sliced_field(ts, snapshot_number, snapshot_number_to_index_map, field,
 
 def plot_phase_diagram(ts, snapshot_number, snapshot_number_to_index_map,
                        SN_times, plots_dir,
+                       weight_field,
+                       field_type="all",
                        save_plot=True,
-                       show_plot=True):
+                       show_plot=True,
+                       seaborn_style="ticks"):
     """ Creates [and optionally saves] a density-temperature phase diagram
 
     Inputs
@@ -368,57 +433,63 @@ def plot_phase_diagram(ts, snapshot_number, snapshot_number_to_index_map,
         - the times that SNe occur for this simulation (in Myr)
     plots_dir : str
         - where plots should be saved, if they are to be saved
+    weight_field : str
+        - the field you'll be creating your 2D histogram of
+          (e.g. `particle_mass`)
+    field_type : Optional(str)
+        - the yt field type (e.g. `all`, `deposit` or your own particle filter)
     save_plot : Optional(bool)
         - True if you want to save plot images
     show_plot : Optional(bool)
         - True if you want to show plots
          (you might want to disable this for non-interactive sessions)
+    seaborn_style : Optional(str)
+        - a valid argument for `seaborn.axes_style`
+
+    Returns
+    -------
+    ppp
 
     """
     i = snapshot_number_to_index_map[snapshot_number]
-    ds = ts[i]
+    ds = load_ds_from_ts(ts, i)
     dd = ds.all_data()
 
-    plot = yt.PhasePlot(dd,
-                        ('gas', 'density'),
-                        ('gas', 'temperature'),
-                        ('gas', 'cell_mass'))
+    ppp = yt.ParticlePhasePlot(dd,
+                               (field_type, "density"),
+                               (field_type, "temperature"),
+                               z_fields=(field_type, weight_field),
+                               x_bins=100,
+                               y_bins=100,)
 
-    plot.set_unit('density', 'g/cm**3')
-    plot.set_unit('cell_mass', 'Msun')
+    if weight_field == "particle_mass":
+        ppp.profile.set_field_unit("particle_mass", "Msun")
+        ppp.plots[(field_type, "particle_mass")].zmin, ppp.plots[(field_type, "particle_mass")].zmax = (None, None)
 
-    # plot.annotate_timestamp(corner="upper_left", draw_inset_box=True)
+    ppp.set_unit("density", "amu/cm**3")
+    ppp.set_xlabel("Density $ [ m_\mathrm{H} \; \mathrm{cm}^{-3} ] $")
 
-    # t = ds.current_time.convert_to_cgs().value / Myr
-    # time_str = format_time_str(t)
-    # plt.text(.2,.94,
-    #                 time_str,
-    #                 # coord_system="axis",
-    #                 inset_box_args={"facecolor":"white",
-    #                                    "alpha":0.9},
-    #                )
-    # N_SNe_so_far = np.sum(t > SN_times)
-    # plt.text(.8,.94,
-    #                 "N_SNe: {}".format(N_SNe_so_far),
-    #                 # coord_system="axis",
-    #                 inset_box_args={"facecolor":"white",
-    #                                    "alpha":0.9},
-    #                )
+    ppp.set_xlim(1e-4, 1e3)
+    ppp.set_ylim(1e1, 1e7)
 
-    plot.set_xlim(1e-28, 1e-22)
-    plot.set_ylim(1, 1e7)
+    ppp.set_log("density", True)
+    ppp.set_log("temperature", True)
+    ppp.set_log("particle_mass", True)
+
+    ppp.set_background_color(weight_field, color="white")
 
     if show_plot:
-        plot.show()
+        with sns.axes_style(seaborn_style):
+            ppp.show()
 
     if save_plot:
         subdir = "phase_plot"
         if not os.path.exists(os.path.join(plots_dir, subdir)):
             os.mkdir(os.path.join(plots_dir, subdir))
-        plot_name = os.path.join(subdir, "snapshot_{:0>3}".format(snapshot_number))
-        yt_plot_saver(plot, plot_name, plots_dir)
+        plot_name = os.path.join(subdir, "snapshot_{:0>3}_{}".format(snapshot_number, weight_field))
+        yt_plot_saver(ppp, plot_name, plots_dir)
 
-    del dd
+    return ppp
 
 
 # ### Profile plotting (requires a bit more low-level programming)
@@ -508,8 +579,7 @@ def format_time_str(current_time):
 def plot_profile(ts, snapshot_number, snapshot_number_to_index_map, field,
                  rho_0, plots_dir,
                  save_plot=True,
-                 show_plot=True,
-                 MHD=False):
+                 show_plot=True):
     """ Creates [and optionally saves] a radial profile plot of `field`
 
     Inputs
@@ -536,7 +606,7 @@ def plot_profile(ts, snapshot_number, snapshot_number_to_index_map, field,
 
     """
     i = snapshot_number_to_index_map[snapshot_number]
-    ds = ts[i]
+    ds = load_ds_from_ts(ts, i)
     sp = ds.sphere(ds.domain_center, ds.domain_width[0]/2)
 
     if field is "density":
